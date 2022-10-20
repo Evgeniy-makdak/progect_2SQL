35a7b6c9dd895b15253d500c67254e210dc5c7ff
Evgeniy Acteck <makdak23@mail.ru>
2022-10-20 21:32:17 +0300

    0.0.1 -v

---
+++ README.md
@@ -0,0 +1,15 @@
+
+Программа на Pithon формирует файл suppliers.sql на основе данных файла suppliers.json,
+который создаёт в БД таблицу suppliers, добавляет новое поле в таблицу products
+и добавляет в него связь с таблицей suppliers
+
+- [x]  Проект выложен на GitHUB
+- [x]  Файл [README.md](http://README.md) оформлен
+- [x]  Есть [дамп](http://DB_main.sql) итоговой БД 
+- [x]  Есть [ER-диаграмма](http://Screenshot.png) итоговой БД
+- [x]  Есть [файл](http://suppliers.sql) sql-скрипта для добавления данных в таблицу suppliers
+- [x]  Запросы из задания оформлены в отдельных файлах
+    - [suppliers_page.sql](http://suppliers_page.sql)
+    - [orders_page.sql](http://orders_page.sql)
+    - [employees_page.sql](http://employees_page.sql)
+    - [products_page.sql](http://products_page.sql)
---
+++ main.py
@@ -0,0 +1,82 @@
+import json
+
+
+def get_data_json():
+    """чтение файла json"""
+    with open('suppliers.json', encoding='utf-8') as input_file:
+        return json.load(input_file)
+
+
+def set_data_to_sql_file(data: str):
+    """ запись запроса в файл sql"""
+    with open('suppliers.sql', 'w', encoding="utf-8") as output_file:
+        output_file.write(data)
+
+
+def get_req_insert():
+    """ формирвание запроса на добавление новых данных со связями в стркуктуру бд"""
+    list_suppliers = get_data_json()
+    req = """
+            ALTER TABLE IF EXISTS products
+                ADD COLUMN IF NOT EXISTS suppliers_id smallint;
+
+            DROP TABLE IF EXISTS suppliers; 
+
+            CREATE TABLE suppliers (
+                suppliers_id smallint NOT NULL,
+                company_name character varying(40) NOT NULL,
+                contact character varying(40),
+                country character varying(80),
+                city character varying(100),
+                phone character varying(24),
+                fax character varying(24),
+                homepage text,
+                products character varying(255)
+
+            );
+"""
+    req_update_products = ""
+    suppliers_id = 0
+    row2 = ""
+    for each_row in list_suppliers:
+        # парсим построчно на значения полей
+        suppliers_id += 1
+        company_name = each_row['company_name'].replace("'", "''")
+        contact = each_row['contact'].replace("'", "''")
+        _address = each_row['address'].split(";")
+        country = _address[0].strip().replace("'", "''")
+        state = _address[1].strip().replace("'", "’")
+        index = _address[2].strip().replace("'", "''")
+        city = _address[3].strip().replace("'", "''")
+        phone = each_row['phone'].replace("'", "''")
+        fax = each_row['fax'].replace("'", "''")
+        homepage = each_row['homepage'].replace("'", "''")
+        list_products = each_row['products']
+
+        list_good_products = [each_product.replace("'", "''") for each_product in list_products]
+
+        products = "','".join(list_good_products)
+        row2 += f"""
+            UPDATE products  SET suppliers_id={suppliers_id}  WHERE product_name IN ('{products}');"""  # заполнение id
+
+        str_products = ",".join(list_good_products)
+        row = f"""
+                    INSERT INTO suppliers VALUES ({suppliers_id},'{company_name}','{country}','{state}','{index}','{city}','{phone}','{fax}','{homepage}','{str_products}');"""
+        req += row
+    req += req_update_products
+    req += row2
+    req += f"""
+
+                    ALTER TABLE ONLY suppliers
+                        ADD CONSTRAINT pk_suppliers PRIMARY KEY (suppliers_id);
+
+                    ALTER TABLE ONLY products
+                        ADD CONSTRAINT fk_suppliers FOREIGN KEY (suppliers_id) REFERENCES suppliers;
+            """
+
+    return req
+
+
+if __name__ == '__main__':
+    req_insert = get_req_insert()
+    set_data_to_sql_file(req_insert)
